10d9b253867599eac86e2c3a7fcd29b798678777
Alejandro Cardenas <alecar.main@gmail.com>
2025-08-14 15:31:12 -0500

    test: fix quality test

--- corporate_partner_access/urls.py
+++ corporate_partner_access/urls.py
@@ -2,8 +2,8 @@
 URLs for corporate_partner_access.
 """
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from corporate_partner_access import views
 
--- corporate_partner_access/utils.py
+++ corporate_partner_access/utils.py
@@ -1,6 +1,8 @@
 """Utility functions for corporate_partner_access, including drf-spectacular hooks."""
 
+
 def spectacular_filter_hook(endpoints):
+    """Filter endpoints to only include those starting with /corporate_access/api/."""
     # Only include endpoints starting with /corporate_access/api/
     return [
         (path, path_regex, method, callback)
