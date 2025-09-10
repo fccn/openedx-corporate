4a89c4b5175b3492cbf24788325d1e6836865075
Andres Felipe Giraldo <andres.giraldo@edunext.co>
2025-09-10 14:20:44 -0500

    fix: quality checks

--- corporate_partner_access/test/grade_factory_backend.py
+++ corporate_partner_access/test/grade_factory_backend.py
@@ -1,11 +1,13 @@
+"""Mock backend for CourseGradeFactory for testing purposes."""
+
 
 class CourseGradeFactoryMock:
     """Mock of CourseGradeFactory for testing purposes."""
 
-    def read(self, user, course_key):
+    def read(self, user, course_key):  # pylint: disable=unused-argument
         """Mock read method."""
         return None
-    
+
 
 def course_grade_factory_class():
     """Return the mock CourseGradeFactory class."""
